d964467375478d57cd2ef7da90b253e99f6a6d1a
Jan Lesage <jan.lesage@vlaanderen.be>
2024-02-09 17:05:45 +0100

    chore: add basic code and starter workfow

---
+++ src/AssociationRegistry.KboMutations.Fetcher/Program.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+var host = builder.Build();
+// host.Run();
+
+Console.Out.WriteLine("MAGDA - KBO Mutations");
