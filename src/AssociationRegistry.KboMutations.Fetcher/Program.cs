65374a1844ecffedd6d261f2f9338c2fab86e863
Jan Lesage <jan.lesage@vlaanderen.be>
2024-02-12 11:12:21 +0100

    chore: add lambda project stubs and paket setup

--- src/AssociationRegistry.KboMutations.Fetcher/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.Extensions.Hosting;
-
-var builder = Host.CreateApplicationBuilder(args);
-
-var host = builder.Build();
-// host.Run();
-
-Console.Out.WriteLine("MAGDA - KBO Mutations");
