5ab4f05e066b396ecefd5a3f4af4bb0daa5b5078
Jan Lesage <jan.lesage@vlaanderen.be>
2024-03-06 15:38:47 +0100

    chore: or-2107 move notification code to AssociationRegistry.KboMutations

--- src/AssociationRegistry.KboMutations.MutationLambdaContainer/Notifications/INotifier.cs
+++
@@ -1,7 +0,0 @@
-namespace AssociationRegistry.KboMutations.MutationLambdaContainer.Notifications;
-
-public interface INotifier
-{
-    Task NotifySuccess(int numberOfFiles);
-    Task NotifyFailure(string reason);
-}
--- src/AssociationRegistry.KboMutations/SsmClientWrapper.cs
+++ src/AssociationRegistry.KboMutations/SsmClientWrapper.cs
@@ -2,7 +2,7 @@ using System.Net;
 using Amazon.SimpleSystemsManagement;
 using Amazon.SimpleSystemsManagement.Model;
 
-namespace AssociationRegistry.KboMutations.MutationLambdaContainer;
+namespace AssociationRegistry.KboMutations;
 
 public class SsmClientWrapper
 {
