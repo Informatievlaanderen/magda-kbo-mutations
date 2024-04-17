64aee753cc4c894ef64c52204412b8917b97472d
Koen Metsu <koen@kometsoft.be>
2024-04-17 10:23:12 +0200

    test: or-2134 test ftpsListItemParser

---
+++ test/AssociationRegistry.KboMutations.MutationLambdaContainer.Tests/FtpsListItemParserTests.cs
@@ -0,0 +1,26 @@
+using Xunit;
+using AssociationRegistry.KboMutations.MutationLambdaContainer.Abstractions;
+using AssociationRegistry.KboMutations.MutationLambdaContainer.Ftps;
+using FluentAssertions;
+using Moq;
+
+namespace AssociationRegistry.KboMutations.MutationLambdaContainer.Tests;
+
+public class FtpsListItemParserTests
+{
+    [Fact]
+    public void ParsesString()
+    {
+        var ftpsClient = new Mock<IFtpsClient>();
+
+        var result = @$"-rw-rw-r--   1 kbbj.vlaanderen.be-dv-verenigingsregister-ftp kbbj.vlaanderen.be-dv-verenigingsregister-ftp        0 Mar 22 05:41 pub_mut_klanten-functies0200_20240322043924000.csv{System.Environment.NewLine}-rw-rw-r--   1 kbbj.vlaanderen.be-dv-verenigingsregister-ftp kbbj.vlaanderen.be-dv-verenigingsregister-ftp     2260 Apr 17 05:18 pub_mut-ondernemingVKBO0200_20240417031451000.xml";
+        ftpsClient.Setup(x => x.GetListing(It.IsAny<string>()))
+            .Returns(
+                result);
+
+
+        var ftpsListItems = FtpsListParser.Parse(new FtpUriBuilder("host", 21), result);
+
+        ftpsListItems.Should().HaveCount(2);
+    }
+}
